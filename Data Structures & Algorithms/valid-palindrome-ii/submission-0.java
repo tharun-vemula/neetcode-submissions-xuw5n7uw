f6323ef82905387fd071e9dc53c3286c905b9516
Tharun <77895048+tharun-vemula@users.noreply.github.com>
2026-06-21 12:30:13 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return isPalindrome(s.substring(0, l) + s.substring(l+1)) || 
+                isPalindrome(s.substring(0, r) + s.substring(r + 1));
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s) {
+        for(int i = 0; i < s.length()/2; i++) {
+            if (s.charAt(i) != s.charAt(s.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
